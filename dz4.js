305a43c0bc77aff7c49d2aa2d1ba514404483dd1
Sergey Pilipchak <sergey.pilipchak@gmail.com>
2016-11-29 18:29:06 +0200

    Update dz4.js
    updated task #5

--- dz4.js
+++ dz4.js
@@ -96,3 +96,5 @@ console.log(countPrimesSumInRange(2000));
 //215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 //What is the sum of the digits of the number 21000?
 		//no idea what this means
+2^15 = 32768;
+2^1000;
